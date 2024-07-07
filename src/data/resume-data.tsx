422255a488cf348f83478f08f8340a450c9f2a38
yasintuncerr <62839963+yasintuncerr@users.noreply.github.com>
2024-07-07 03:15:14 +0300

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,268 +20,197 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Yasin Tunçer",
+  initials: "YT",
+  location: "Bursa, Turkey",
+  locationLink: "https://www.google.com/maps/place/Bursa",
   about:
-    "Detail-oriented Full Stack Engineer dedicated to building high-quality products.",
+    "Half Full Stack Engineer, AI and machine learning developer, and DevOps enthusiast. Passionate about leveraging technology to build innovative and high-quality solutions.",
   summary:
-    "As a Full Stack Engineer, I specialize in taking products from concept to launch. I excel in leading teams and creating environments where individuals perform at their best. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience working remotely with companies worldwide.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "As an AI and Machine Learning Engineer, I focus on developing and implementing AI models and computer vision algorithms. I excel in creating and managing infrastructure, handling DevOps tasks, and integrating advanced AI-based tools. My expertise includes Python, C++, Pytorch, Docker, and Linux. I have worked on various projects such as Imagine, Repeater, Upscaler, and Archivist. I am always eager to learn new technologies and improve my skills, thriving in environments where collaboration and innovation are encouraged.",
+  avatarUrl: "https://avatars.githubusercontent.com/yasintuncerr",
+  personalWebsiteUrl: "https://www.yasintuncer.net",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "yasintuncerr@gmail.com",
+    tel: "---",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/yasintuncerr",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/yasintuncer/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/gauss_kafa",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Bursa Uludag University",
+      degree: "Bachelor's Degree in Electronic Engineering",
+      start: "2012",
+      end: "2018",
     },
+    {
+      school: "Bursa Technical University",
+      degree: "Master's Degree in Computer Engineering",
+      start: "2024",
+      end:"present"
   ],
   work: [
     {
-      company: "Film.io",
-      link: "https://film.io",
-      badges: ["Remote"],
-      title: "Software Architect",
-      logo: ConsultlyLogo,
-      start: "2024",
-      end: null,
-      description:
-        "Leading the development of the Film.io platform. Technologies: React, TypeScript, Node.js",
-    },
-    {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
-      description:
-        "Implemented new features, led a squad, improved code delivery process, and initiated migration from Emotion to Tailwind CSS. Technologies: React, TypeScript, GraphQL",
-    },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, and AAA. Built a live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
+      company: "A.I.T",
+      link: "https://ai.ait.com.tr",
       badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Led the Android team and developed apps for major Polish companies like LOT, Polskie Radio, Agora, and PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
+      title: "Full stack Developer",
       logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Developed and tested software for LTE base stations",
+      start: "2018",
+      end: "present",
+      description: "Developed AI models and computer vision algorithms. Created and managed infrastructure. Handled DevOps tasks. Technologies: Linux, C++, Python, Pytorch, Pytorch TorchServe, Nvidia TritonServer, Nvidia TensorRT, Docker, MongoDB",
     },
   ],
   skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "C",
+    "C++",
+    "Python",
+    "Computer Vision",
+    "Deep Learning",
+    "Machine Learning",
+    "Linux",
+    "Docker",
+    "Bash Script",
   ],
   projects: [
     {
-      title: "Parabol",
+      title: "Aitools",
       techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
+        "Python",
         "Node.js",
-        "GraphQL",
+        "Uix",
+        "Pytorch",
+        "Generative AI",
+        "Computer Vision",
+        "Nvidia TritonServer",
+        "Docker",
+        "Linux",
+        "DevOps",
+        "MongoDB",
+        "Compute Server Infrastructure"
       ],
       description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
+        "The AI Tools product suite includes a variety of AI-based tools tailored to the needs of the textile design industry. Users can generate new creative patterns with these tools.",
       logo: ParabolLogo,
       link: {
-        label: "github.com",
-        href: "https://parabol.co/",
+        label: "aitools",
+        href: "https://ai.ait.com.tr/",
       },
     },
     {
-      title: "Evercast",
+      title: "Imagine",
       techStack: [
-        "Lead Frontend Developer",
-        "TypeScript",
-        "React",
+        "Python",
+        "Pytorch",
+        "Generative AI",
+        ],
+      description:
+        "The Imagine tools, part of the AI Tools product suite, allow users to generate new creative digital textile design patterns.",
+      logo: ParabolLogo,
+      link: {
+        label: "Imagine",
+        href: "https://ai.ait.com.tr/project/ai-pattern-generator/",
+      },
+    },
+    {
+      title: "Repeater",
+      techStack: [
+        "Python",
+        "Pytorch",
+        "Generative AI",
+        "Computer Vision"
+        ],
+      description:
+        "The Repeater tools, part of the AI Tools product suite, convert unaligned digital textile design patterns into seamlessly repeating patterns, making them ready for conventional printing.",
+      logo: ParabolLogo,
+      link: {
+        label: "Repeater",
+        href: "https://ai.ait.com.tr/project/repeat-pattern-generator/",
+      },
+    },
+    {
+      title: "Upscaler",
+      techStack: [
+        "Python",
+        "Pytorch",
+        "Generative AI",
+        "Computer Vision"
+        ],
+      description:
+        "The Upscaler tools, part of the AI Tools product suite, enhance low-quality digital textile design patterns into high-quality versions.",
+      logo: ParabolLogo,
+      link: {
+        label: "Repeater",
+        href: "https://ai.ait.com.tr/project/ai-image-upscaler/",
+      },
+    },
+    {
+      title: "Archivist",
+      techStack: [
+        "Python",
         "Node.js",
-        "GraphQL",
+        "Pytorch",
+        "Machine Learning",
+        "Deep Learning",
+        "Computer Vision",
+        "Pytorch TorchServe",
+        "Docker",
+        "Linux",
+        "DevOps"
       ],
       description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
+        "The Archivist classifies and organizes huge textile pattern archives. It provides a web-based interface for previewing, filtering, and searching patterns using AI-based image search, designed for textile designers and manufacturers.",
+      logo: ParabolLogo,
       link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
+        label: "archivist",
+        href: "https://ai.ait.com.tr/project/archivist/",
       },
     },
     {
-      title: "Consultly",
+      title: "Fabric Defect Detection",
       techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "C++",
+        "Qt",
+        "QML",
+        "Computer Vision",
+        "Cross Platform",
+        ],
+      description:
+        "A real-time system that detects fabric defects before digital printing",
+      logo: ParabolLogo,
+      link: {
+        label: "archivist",
+        href: "https://ai.ait.com.tr/project/archivist/",
+      },
+    },
+    {
+      title: "Psd SDK",
+      techStack: [
+        "C++",
+        "Computer Vision",
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
       description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
+        "The PSD Files Multi-Channel Handler has been improved. The base project was forked from MolecularMatters/psd_sdk.",
+      logo: ParabolLogo,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "psd_sdk",
+        href: "https://github.com/yasintuncer/psd_sdk",
       },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "Personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers, and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description: "Howdy is a place for joining communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
